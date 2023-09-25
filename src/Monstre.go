81fd4c954ac3a4e12995b9e123e2e39af07e253a
thierry mousnier <thierry.mousnier@ynov.com>
2023-09-25 16:45:44 +0200

    faux code

--- src/Monstre.go
+++ src/Monstre.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Monstre struct {
-	name string
-	pvmax int
-	pvnow int
+	name     string
+	pvmax    int
+	pvnow    int
 	pointatt int
 }
 
@@ -22,4 +22,20 @@ func (c *Monstre) DeadMonstre() {
 	if c.pvnow <= 0 {
 		fmt.Println("tu a pulvérisé la Carrie, Bien joué !!!")
 	}
+}
+
+func (p *Perso) GoblinPattern(c Monstre) {
+	var damage int
+	for p.pvnow > 0 {
+		if c.tour%3 == 0 {
+			damage = 2 * c.pointatt
+		} else {
+			damage = c.pointatt
+		}
+	}
+	p.pvnow -= damage
+
+	fmt.Printf("Tour %d: La carrie inflige %d dégats a toi ma sucrerie, Points de vie restants : %d\n", c.tour, c.pointatt, p.pvnow)
+
+	c.tour++
 }
