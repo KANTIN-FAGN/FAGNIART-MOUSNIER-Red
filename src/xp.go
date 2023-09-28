96fd04471554ed3efe188dccaecfc03b34184d8c
thierry mousnier <thierry.mousnier@ynov.com>
2023-09-28 16:34:55 +0200

    xp

--- src/xp.go
+++ src/xp.go
@@ -12,5 +12,11 @@ func (p *Perso) Xp() {
 		fmt.Println("Level : ", p.lvl)
 		p.xpmax += 19
 		p.xp = a
+		p.pvmax += 5
+		p.pvnow += 5
+		p.AddInventory("éclat de sucre vivifiant")
+		p.AddInventory("éclat de sucre vivifiant")
+	} else {
+		fmt.Println("vous avez", p.xp, "XP sur", p.xpmax, "XP pour passer le level :", p.lvl)
 	}
 }
